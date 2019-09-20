62ea7847d8e1901f5baa72137a4a3f2b6b7f31ad
nairadi <nairadi94@gmail.com>
2019-09-20 02:11:26 -0400

    Initial commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(SceneManager.sceneCountInBuildSettings > currentSceneIndex + 1 ? currentSceneIndex + 1 : 0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
